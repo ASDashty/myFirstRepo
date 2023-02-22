251745f2e2918dfc689e9a7a568fdcc20241608f
ASDashty <99809904+ASDashty@users.noreply.github.com>
2023-02-21 20:06:58 -0500

    Update GitPracticeTwo.java
    This is pull practice check

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -7,7 +7,7 @@ public class GitPracticeTwo {
 
 		System.out.println("This is for push practice");
 		System.out.println("This is for pull practice");
-		System.out.println("This is practice for pull);
+		System.out.println("This is practice for pull");
 	}
 
 }
