c005a372ac476d54e76c4db6924739ecc8fe912d
Shaheer <shaheer.dashty@gmail.com>
2023-02-21 19:52:56 -0500

    This is push practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -1,16 +1,11 @@
 package test;
 
-public class UnitTest {
+public class GitPracticeTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a = 10;
-		int b = 15;
-		int c = a+b;
-		
-		System.out.println(c);
-		
+		System.out.println("This is for push practice");
 	}
-	
+
 }
